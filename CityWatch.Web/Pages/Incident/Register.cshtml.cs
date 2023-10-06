b5e0475afe98d65e1da35ac6a29620b92c22c9d2
Dileep <addileepsebastian@gmail.com>
2023-10-06 11:22:09 +0530

    157_ir PDf Fine name Added

--- CityWatch.Web/Pages/Incident/Register.cshtml.cs
+++ CityWatch.Web/Pages/Incident/Register.cshtml.cs
@@ -287,6 +287,7 @@ namespace CityWatch.Web.Pages.Incident
                     messageHtml = messageHtml + "<p>Please " +
                     "<a href=\" https://c4istorage1.blob.core.windows.net/irfiles/" + (new string(blobName.Take(8).ToArray()) + "/" + blobName) + "\" target=\"_blank\">" +
                     "click here</a> to download the Incident Report</p>";
+                    messageHtml = messageHtml + "<p>File name : "+ blobName+"</p>";
                 }
 
             }
