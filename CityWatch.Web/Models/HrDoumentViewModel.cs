5b3d03de056e3bceef11d03ed2bd6e2dc1871286
Binoy-Lonappan <103546455+Binoycl@users.noreply.github.com>
2024-07-01 13:11:41 +0530

    P1# 213 O Binoy

--- CityWatch.Web/Models/HrDoumentViewModel.cs
+++ CityWatch.Web/Models/HrDoumentViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CityWatch.Data.Models;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
@@ -25,8 +26,7 @@ namespace CityWatch.Web.Models
         public int referenceNoAlphabetId { get; set; }
         public int referenceNoNumberId { get; set; }
         public int hrGroupId { get; set; }
-        
-
+        public string ClientSitesSummary { get; set; }
 
 
         public static HrDoumentViewModel FromDataModel(HrSettings hrSettings)
@@ -35,7 +35,7 @@ namespace CityWatch.Web.Models
             {
                 Id = hrSettings.Id,                
                 ClientTypes = hrSettings.hrSettingsClientSites!=null? string.Join(", ", hrSettings.hrSettingsClientSites.Select(z => z.ClientSite.ClientType.Name).Distinct()):string.Empty,
-                ClientSites = hrSettings.hrSettingsClientSites != null ? string.Join(", ", hrSettings.hrSettingsClientSites.Select(z => z.ClientSite.Name).Distinct()) : string.Empty,
+                ClientSites = hrSettings.hrSettingsClientSites != null ? string.Join("<br>", hrSettings.hrSettingsClientSites.Select(z => z.ClientSite.Name).Distinct()) : string.Empty,
                 States = hrSettings.hrSettingsClientStates != null ? string.Join(", ", hrSettings.hrSettingsClientStates.Select(z => z.State.Trim()).Distinct()) : string.Empty,
                 GroupName = hrSettings.GroupName,
                 referenceNo = hrSettings.ReferenceNo,
@@ -43,8 +43,20 @@ namespace CityWatch.Web.Models
                 referenceNoAlphabetsName= hrSettings.ReferenceNoAlphabets.Name,
                 referenceNoNumberId = hrSettings.ReferenceNoNumberId,
                 referenceNoAlphabetId = hrSettings.ReferenceNoAlphabetId,
-                hrGroupId = hrSettings.HRGroupId
+                hrGroupId = hrSettings.HRGroupId,
+                ClientSitesSummary = hrSettings.hrSettingsClientSites != null ? GetFormattedClientSites(hrSettings.hrSettingsClientSites) : string.Empty
             };
+        }
+
+        private static string GetFormattedClientSites(IEnumerable<HrSettingsClientSites> hrSettingsClientSites)
+        {
+            var clientSites = hrSettingsClientSites.Select(x => x.ClientSite.Name).OrderBy(x => x);
+            if (clientSites.Count() == 0)
+                return "";
+            if (clientSites.Count() <= 2)
+                return string.Join("<br>", clientSites);
+
+            return $"{string.Join("<br>", clientSites.Take(2))} <br><span class=\"text-primary\"> and {clientSites.Count() - 2} more sites </span>";
         }
     }
 
