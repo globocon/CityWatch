d521098b9953ea458283c4035abb02ce641fa435
Dileep Sebastian <42341364+dill3032@users.noreply.github.com>
2025-01-01 23:47:38 +0530

    P2#148Adhoc Shift Half Done in step2

---
+++ CityWatch.Data/Models/ClientSiteManningKpiSettingADHOC.cs
@@ -0,0 +1,68 @@
+﻿using iText.Svg.Renderers.Impl;
+using Org.BouncyCastle.Asn1.Cms;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace CityWatch.Data.Models
+{
+    public class ClientSiteManningKpiSettingADHOC
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int SettingsId { get; set; }
+        [Required]
+        public int PositionId { get; set; }
+        [ForeignKey("PositionId")]
+        [JsonIgnore]
+        public DayOfWeek WeekDay { get; set; }
+
+        [ForeignKey("SettingsId")]
+        [JsonIgnore]
+
+        public ClientSiteKpiSetting ClientSiteKpiSetting { get; set; }
+
+        public int? NoOfPatrols { get; set; }
+
+        public string EmpHoursStart { get; set; }
+
+        public string EmpHoursEnd { get; set; }
+
+        public string Type { get; set; }
+        [NotMapped]
+        public bool DefaultValue { get; set; }
+
+        public int? OrderId { get; set; }
+        [NotMapped]
+        public double SumOfHours
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(EmpHoursEnd) && !string.IsNullOrEmpty(EmpHoursStart))
+                {
+
+                    var totalMin = (TimeSpan.Parse(EmpHoursEnd + ":00") - TimeSpan.Parse(EmpHoursStart + ":00")).TotalMinutes;
+                    var hours = Math.Ceiling(totalMin / 60);
+                    return hours;
+                }
+                else
+                {
+                    return 0.0;
+                }
+
+
+            }
+
+
+        }
+
+
+        public int? IsPHO { get; set; }
+
+        public string CrmSupplier { get; set; }
+    }
+
+}
