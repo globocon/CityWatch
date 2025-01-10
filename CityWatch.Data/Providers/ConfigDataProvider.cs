4b785b0582a71d47bcab0364ccfa59c8dc14c6cd
Dileep Sebastian <42341364+dill3032@users.noreply.github.com>
2025-01-10 11:44:25 +0530

    chnages done

--- CityWatch.Data/Providers/ConfigDataProvider.cs
+++ CityWatch.Data/Providers/ConfigDataProvider.cs
@@ -346,52 +346,7 @@ namespace CityWatch.Data.Providers
             return staffDocList;
         }
 
-        //public List<StaffDocument> GetStaffDocumentsUsingType(int type, string query)
-        //{
-        //    // Retrieve documents of the specified type and optionally filter by query
-        //    var staffDocList = _context.StaffDocuments
-        //        .Where(x => x.DocumentType == type &&
-        //                    (string.IsNullOrEmpty(query) || x.FileName.Contains(query)))
-        //        .ToList();
-
-        //    // Check if any of the documents have a ClientSite assigned
-        //    bool hasClientSite = staffDocList.Any(doc => doc.ClientSite.HasValue);
-
-        //    if (hasClientSite)
-        //    {
-        //        foreach (var doc in staffDocList)
-        //        {
-        //            if (doc.ClientSite.HasValue)
-        //            {
-        //                // Fetch the ClientSite along with ClientType using Include
-        //                var clientSite = _context.ClientSites
-        //                    .Where(x => x.Id == doc.ClientSite)
-        //                    .Include(x => x.ClientType)
-        //                    .FirstOrDefault();
-
-        //                // Set the properties if ClientSite is found
-        //                if (clientSite != null)
-        //                {
-        //                    doc.ClientSiteName = clientSite.Name ?? "Unknown";
-        //                    doc.ClientTypeName = clientSite.ClientType?.Name ?? "Unknown";
-        //                }
-        //            }
-        //        }
-
-        //        // Sort the staff document list by ClientTypeName and ClientSiteName
-        //        staffDocList = staffDocList
-        //            .OrderBy(x => x.ClientTypeName)
-        //            .ThenBy(x => x.ClientSiteName)
-        //            .ToList();
-        //    }
-        //    else
-        //    {
-        //        // If no ClientSite, just order by FileName
-        //        staffDocList = staffDocList.OrderBy(x => x.FileName).ToList();
-        //    }
-
-        //    return staffDocList;
-        //}
+        
 
         public void SaveStaffDocument(StaffDocument staffdocument)
         {
