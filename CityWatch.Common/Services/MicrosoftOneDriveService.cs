e5ee5a86e628f680ef904d4e57110df59958bca9
Jisha James <jishakallani@gmail.com>
2025-08-18 09:18:18 +0530

    p5-displaying ppts

--- CityWatch.Common/Services/MicrosoftOneDriveService.cs
+++ CityWatch.Common/Services/MicrosoftOneDriveService.cs
@@ -63,7 +63,7 @@ namespace CityWatch.Common.Services
             using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
                 // STEP 1: Create an upload session
-                 var uploadSession = await graphClient.Me.Drive.ItemWithPath("largefile.zip").CreateUploadSession.PostAsync();
+                var uploadSession = await graphClient.Me.Drive.Root.ItemWithPath("largefile.zip").CreateUploadSession.PostAsync(new CreateUploadSessionPostRequestBody());
                 // STEP 2: Upload the file in chunks
                 int maxSliceSize = 320 * 1024; // 320KB per chunk (can be bigger, e.g., 5MB)
                 //var uploadProvider = new ChunkedUploadProvider(uploadSession, graphClient, fileStream, maxSliceSize);
