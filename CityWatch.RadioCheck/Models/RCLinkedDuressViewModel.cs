8452b2765451fabea24aeb5979ccee2b1a6d660b
Jisha James <jishakallani@gmail.com>
2026-01-13 09:40:21 +0530

    p4-136-linked-services

--- CityWatch.RadioCheck/Models/RCLinkedDuressViewModel.cs
+++ CityWatch.RadioCheck/Models/RCLinkedDuressViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CityWatch.Data.Models;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
@@ -22,7 +23,7 @@ namespace CityWatch.Kpi.Models
         [Display(Name = "Group Name")]
         public string GroupName { get; set; }
 
-        
+        public string[] LinkedServices { get; set; }
 
         public static RCLinkedDuressMaster ToDataModel(RCLinkedDuressViewModel viewModel)
         {
@@ -31,12 +32,33 @@ namespace CityWatch.Kpi.Models
                 Id = viewModel.Id,
                 GroupName= viewModel.GroupName,
                 RCLinkedDuressClientSites = viewModel.ClientSiteIds.Select(z => new RCLinkedDuressClientSites() { RCLinkedId = viewModel.Id, ClientSiteId = z }).ToList(),
-               
+                IsLB = viewModel.LinkedServices != null && viewModel.LinkedServices.Any(s => string.Equals(s, "LB", StringComparison.OrdinalIgnoreCase)),
+                IsKV = viewModel.LinkedServices != null && viewModel.LinkedServices.Any(s => string.Equals(s, "KV", StringComparison.OrdinalIgnoreCase)),
+                IsSW = viewModel.LinkedServices != null && viewModel.LinkedServices.Any(s => string.Equals(s, "SW", StringComparison.OrdinalIgnoreCase)),
+
             };
         }
 
         public static RCLinkedDuressViewModel FromDataModel(RCLinkedDuressMaster dataModel)
         {
+            //return new RCLinkedDuressViewModel()
+            //{
+            //    Id = dataModel.Id,
+            //    ClientSiteIds = dataModel.RCLinkedDuressClientSites.Select(z => z.ClientSiteId).ToArray(),
+            //    ClientTypes = string.Join(", ", dataModel.RCLinkedDuressClientSites.Select(z => z.ClientSite.ClientType.Name).Distinct()),
+            //    ClientSites = string.Join(", ", dataModel.RCLinkedDuressClientSites.Select(z => z.ClientSite.Name).Distinct()),
+            //    GroupName = dataModel.GroupName,
+
+            //};
+            var linkedServices = new List<string>();
+
+            if (dataModel.IsLB)
+                linkedServices.Add("LB");
+            if (dataModel.IsKV)
+                linkedServices.Add("KV");
+            if (dataModel.IsSW)
+                linkedServices.Add("SW");
+
             return new RCLinkedDuressViewModel()
             {
                 Id = dataModel.Id,
@@ -44,8 +66,9 @@ namespace CityWatch.Kpi.Models
                 ClientTypes = string.Join(", ", dataModel.RCLinkedDuressClientSites.Select(z => z.ClientSite.ClientType.Name).Distinct()),
                 ClientSites = string.Join(", ", dataModel.RCLinkedDuressClientSites.Select(z => z.ClientSite.Name).Distinct()),
                 GroupName = dataModel.GroupName,
-              
+                LinkedServices = linkedServices.ToArray()
             };
+
         }
     }
 
