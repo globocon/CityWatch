fe1cfb89cd896636138c70a8460ad897ee89022a
Dileep Sebastian <42341364+dill3032@users.noreply.github.com>
2026-01-19 16:14:15 +0530

    Fix case sensitivity for rcPushMessageId to consistently hide guard initials

--- CityWatch.Web/wwwroot/js/guardlog.js
+++ CityWatch.Web/wwwroot/js/guardlog.js
@@ -1381,7 +1381,8 @@ $(function () {
             { field: 'notes', title: 'Event / Notes', width: 450 },
             {
                 field: 'guardInitials', title: 'Guard Initials', width: 80, renderer: function (value, record) {
-                    if (record.rcPushMessageId && record.rcPushMessageId > 0) {
+                    var pushId = record.rcPushMessageId || record.RcPushMessageId;
+                    if (pushId && pushId > 0) {
                         return '';
                     }
                     var rtn = '';
@@ -1429,8 +1430,9 @@ $(function () {
                 },
                 {
                     field: 'guardInitials', title: 'Guard Initials', width: 70, renderer: function (value, record) {
-                        //return record.guardLogin ? record.guardLogin.guard.initial : '';
-                        if (record.rcPushMessageId && record.rcPushMessageId > 0) {
+                        //check both cases
+                        var pushId = record.rcPushMessageId || record.RcPushMessageId;
+                        if (pushId && pushId > 0) {
                             return record.gpsCoordinates ? `<a href="https://www.google.com/maps?q=${record.gpsCoordinates}" target="_blank" data-toggle="tooltip" title=""><i class="fa fa-map-marker" aria-hidden="true"></i></a>` : '';
                         }
                         var rtn = '';
