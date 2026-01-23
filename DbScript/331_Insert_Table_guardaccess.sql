003c55608221d0405a399cafa37bf543e001298a
Jisha James <jishakallani@gmail.com>
2026-01-23 09:50:40 +0530

    p1-339-Security concern with roster

---
+++ DbScript/331_Insert_Table_guardaccess.sql
@@ -0,0 +1,8 @@
+select * from guardaccess
+
+insert into  guardaccess (AccessName ) values ('ADMIN - Roster')
+update guardaccess set CredentialOrder=13 where id=18
+update guardaccess set CredentialOrder=14 where id=11
+update guardaccess set CredentialOrder=15 where id=12
+update guardaccess set CredentialOrder=16 where id=13
+update guardaccess set CredentialOrder=17 where id=17
