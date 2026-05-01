796f99fc31ea35e391f1e0a42bf4b9f427c4791e
dill3032 <addileepsebastian@gmail.com>
2026-05-01 16:07:13 +0530

    Relf Gurad Purple

--- CityWatch.Web/Pages/roster/Booking.cshtml.cs
+++ CityWatch.Web/Pages/roster/Booking.cshtml.cs
@@ -468,11 +468,6 @@ namespace CityWatch.Web.Pages.roster
                 else if (shiftType == "Adhoc") { finalShiftType = "Adhoc"; finalStatus = RosterShiftStatus.Pushed; }
                 else { finalShiftType = "Regular"; finalStatus = RosterShiftStatus.Pushed; }
 
-                if (reliefGuardId.HasValue || !string.IsNullOrEmpty(reliefProviderName))
-                {
-                    finalStatus = RosterShiftStatus.Accepted;
-                }
-
                 existing.ShiftType = finalShiftType;
                 existing.Status = finalStatus;
 
@@ -546,11 +541,6 @@ namespace CityWatch.Web.Pages.roster
                 else if (shiftType == "Declined") { finalShiftType = "Regular"; finalStatus = RosterShiftStatus.Declined; }
                 else if (shiftType == "Adhoc") { finalShiftType = "Adhoc"; finalStatus = RosterShiftStatus.Pushed; }
                 else { finalShiftType = "Regular"; finalStatus = RosterShiftStatus.Pushed; }
-
-                if (reliefGuardId.HasValue || !string.IsNullOrEmpty(reliefProviderName))
-                {
-                    finalStatus = RosterShiftStatus.Accepted;
-                }
 
                 var schedule = new RosterSchedule
                 {
